56e26d399618bf399952a4a9dfcc59054ba11e75
SwadDeeHawSudLoa <127951003+SwadDeeHawSudLoa@users.noreply.github.com>
2024-10-30 15:50:12 +0700

    update

--- app/api/mypost/[id]/route.ts
+++ app/api/mypost/[id]/route.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+// Force dynamic rendering
+export const dynamic = "force-dynamic";
 export async function GET(
   request: Request,
   { params }: { params: { id: string } },
