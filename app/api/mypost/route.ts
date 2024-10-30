1447031da62683fec8242f898bdce43d1dd53bed
SwadDeeHawSudLoa <127951003+SwadDeeHawSudLoa@users.noreply.github.com>
2024-10-30 15:33:34 +0700

    dfdfd

--- app/api/mypost/route.ts
+++ app/api/mypost/route.ts
@@ -1,29 +1,22 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+// Force dynamic rendering
+export const dynamic = "force-dynamic";
 
-export async function GET(request: NextRequest) {
-  try {
-    // Use request.nextUrl to access the query parameters
-    const { searchParams } = request.nextUrl;
-    const user_id = searchParams.get("user_id");
+export async function GET(request: Request) {
+  // Get user_id from the request query parameters
+  const { searchParams } = new URL(request.url);
+  const user_id = searchParams.get("user_id");
 
-    // If user_id is provided, filter posts by user_id
-    const posts = user_id
-      ? await prisma.post.findMany({
-          where: { userIdEdit: user_id },
-        })
-      : await prisma.post.findMany();
+  // If user_id is provided, filter posts by user_id
+  const posts = user_id
+    ? await prisma.post.findMany({
+        where: { userIdEdit: user_id },
+      })
+    : await prisma.post.findMany();
 
-    return NextResponse.json(posts);
-  } catch (error) {
-    console.error("Error retrieving posts:", error);
-    return NextResponse.json(
-      { error: "Error retrieving posts" },
-      { status: 500 }
-    );
-  } finally {
-    await prisma.$disconnect();
-  }
+  await prisma.$disconnect();
+  return NextResponse.json(posts);
 }
