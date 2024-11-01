91c8a12f7a15057e8c64d29823e0cbdeac0fa29c
SwadDeeHawSudLoa <127951003+SwadDeeHawSudLoa@users.noreply.github.com>
2024-11-01 19:25:01 +0700

    update login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -67,29 +67,29 @@ const Login = () => {
   };
 
   return (
-    <section className="bg-gray-50">
+    <section className="bg-gray-700 ">
       <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
         <a
           href="#"
-          className="mb-6 flex items-center text-2xl font-semibold text-gray-900"
+          className="mb-6 flex items-center text-2xl font-semibold text-gray-100"
         >
           <img
-            className="mr-2 h-8 w-8"
-            src="https://s3.getstickerpack.com/storage/uploads/sticker-pack/keqing/sticker_7.png?4996771f86fd38e818153e52edcc458f"
+            className="mr-2 h-8 w-8 text-gray-100"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Emblem_of_Thammasat_University.svg/1024px-Emblem_of_Thammasat_University.svg.png"
             alt="logo"
           />
           TuItemFinder
         </a>
-        <div className="w-full rounded-lg bg-orange-300 shadow sm:max-w-md md:mt-0 xl:p-0 dark:border dark:border-gray-700 dark:bg-gray-800">
+        <div className="w-full rounded-lg bg-gray-100 shadow sm:max-w-md md:mt-0 xl:p-0 dark:border">
           <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 className="text-black text-xl font-bold leading-tight tracking-tight  md:text-2xl ">
               Sign in
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleLogin}>
               <div>
                 <label
                   htmlFor="username"
-                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+                  className="mb-2 block text-sm font-medium text-gray-900 "
                 >
                   Your username
                 </label>
@@ -97,7 +97,7 @@ const Login = () => {
                   type="text"
                   name="username"
                   id="username"
-                  className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                  className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm "
                   placeholder="username"
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
@@ -107,7 +107,7 @@ const Login = () => {
               <div>
                 <label
                   htmlFor="password"
-                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+                  className="mb-2 block text-sm font-medium text-gray-900 "
                 >
                   Password
                 </label>
@@ -116,7 +116,7 @@ const Login = () => {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+                  className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 sm:text-sm "
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   required
