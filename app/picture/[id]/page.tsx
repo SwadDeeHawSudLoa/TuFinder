44e8d07212bd0c7f468440024834e1b068edd2fc
SwadDeeHawSudLoa <127951003+SwadDeeHawSudLoa@users.noreply.github.com>
2024-11-11 13:08:38 +0700

    Update page.tsx

--- app/picture/[id]/page.tsx
+++ app/picture/[id]/page.tsx
@@ -10,24 +10,10 @@ import { LatLngTuple } from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 const picturePage = ({ params }: { params: { id: string } }) => {
-    const inputRef = React.useRef<HTMLInputElement>(null);
-    const router = useRouter();
-    const { id } = params;
- 
+   
     return (
 <>
 <Navbar />
-
-<div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
-<div className="mb-4 flex justify-between">
-    <input
-      ref={inputRef}
-      type="file"
-      className="file-input file-input-bordered file-input-info w-full max-w-xs"
-    />
-  </div>
-    
-</div>
 
 
 
