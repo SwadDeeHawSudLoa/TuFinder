e61b1bedfa898d3645cecc6589668e37e0a40bb7
Sathienphong Ketviwat <130156331+Sathienphong@users.noreply.github.com>
2024-12-12 14:07:53 +0700

    New ui

--- app/component/navbar.tsx
+++ app/component/navbar.tsx
@@ -137,7 +137,7 @@ const Navbar: React.FC = () => {
             ) : (
               <a
                 href="/login"
-                className=" text-sm flex w-full items-center rounded-lg p-3 hover:bg-gray-100"
+                className=" text-sm flex w-full items-center rounded-lg p-3 hover:bg-green-500"
               >
                 <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
@@ -174,7 +174,7 @@ const Navbar: React.FC = () => {
             ) : (
               <a
                 href="/login"
-                className="flex w-full items-center rounded-lg p-3 hover:bg-gray-100"
+                className="flex w-full items-center rounded-lg p-3 hover:bg-green-500"
               >
                 <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
